0ef5a57ae09f27038d04c5b9fe563296592937b0
alexalmeida52 <jhosealex@gmail.com>
2017-10-26 15:05:56 -0300

    Está dando certo.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-# Create your views here.
+# Create your views here. 12345
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
